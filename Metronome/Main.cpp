20a729c11a27714d9f14660ee9897b9e30cd4213
Louis <Workmanlad2@outlook.com>
2023-12-07 14:33:47 -0300

    Initial commit.

--- Metronome/Main.cpp
+++ Metronome/Main.cpp
@@ -1,14 +1,8 @@
-#include <Metronome.h>
-#include <ConsoleScreen.h>
 #include <iostream>
 using std::wcout, std::wcin, std::endl;
 
 int main() {
-    Metronome metronome;
-    ConsoleScreen screen;
-
-    metronome.SetBPM(100);
-    screen.Display(metronome);
+    
 
     return 0;
 }
